b6815dd457162dc6b8b6e98d88202ad87ca9c856
sbcesar <sbcesar@iesrafaelalberti.es>
2024-04-23 19:28:08 +0200

    first commit

---
+++ src/main/kotlin/FromResources.kt
@@ -0,0 +1,13 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.singleWindowApplication
+
+fun main() = singleWindowApplication {
+    Image(
+        painter = painterResource("sample.png"),
+        contentDescription = "Sample",
+        modifier = Modifier.fillMaxSize()
+    )
+}
